a10104d7976cee210e4ef2aeedb22b559b0d3fd8
Willson Guayanay <wilson.guayanay@epn.edu.ec>
2024-06-12 17:59:33 -0500

    Crear múltiples cursores 4

--- src/02-multi-cursor/04-multi-cursor-formato.ts
+++ src/02-multi-cursor/04-multi-cursor-formato.ts
@@ -7,11 +7,14 @@
 //*         ⌥ ⌘ ↑ / ↓
 //*         Ctrl + Alt+ ↑ / ↓
 //* 
+//*     el tip son la barra de espacios!
+//* 
 
-const hulk = 'brouce banner';
-const Hawkeye = 'cinton francis';
-const ironman = 'tony stark';
-const spiderman = 'peter parker';
+
+const hulk       = 'brouce banner';
+const Hawkeye    = 'cinton francis';
+const ironman    = 'tony stark';
+const spiderman  = 'peter parker';
 const viudaNegra = 'natalia romanova';
 
 
